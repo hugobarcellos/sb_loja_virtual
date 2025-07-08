06ce78d65860f8aebfa630ee9913b80e33db69ea
Hugo Barcellos <hugo.barcellos@haytek.com.br>
2025-07-08 16:27:10 -0300

    ajustes

---
+++ agendador.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Ativa o ambiente virtual e roda o dbt
+
+REM Caminho da pasta do projeto
+cd C:\Git\sb_loja_virtual
+
+REM Ativa o ambiente virtual do Python (ajuste se estiver diferente)
+call "C:\Git\sb_loja_virtual\.venv\Scripts\activate.bat"
+
+REM Executa o DBT
+dbt run --profiles-dir "C:\Git\sb_loja_virtual"
+
+REM (opcional) Salva o log num arquivo pra auditoria
+REM dbt run --profiles-dir "C:\Users\HTK1511\.dbt" > dbt_log.txt 2>&1
