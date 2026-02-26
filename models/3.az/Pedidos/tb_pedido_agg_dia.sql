8d1bd02409bb15840218d7234a3f4cf04b4f8113
Hugo Barcellos <hugo.barcellos@haytek.com.br>
2026-02-26 14:43:18 -0300

    wip

--- models/3.az/Pedidos/tb_pedido_agg_dia.sql
+++ models/3.az/Pedidos/tb_pedido_agg_dia.sql
@@ -39,18 +39,21 @@ with cte_pedido as (
 
 , cte_pedido_agg as (
   select dt_pedido
-        ,count(distinct cd_pedido) qt_pedido
-        ,sum(qt_item)              qt_item
-        ,sum(vl_total_item)        vl_total_item
-        ,sum(vl_desconto_rateio)   vl_desconto_rateio
-        ,sum(vl_frete_rateio)      vl_frete_rateio
-        ,sum(vl_total_pedido)      vl_total_pedido
-        ,sum(vl_custo_pedido)      vl_custo_pedido
-        ,min(dt_pedido) over ()    dt_pedido_min
-        ,max(dt_pedido) over ()    dt_pedido_max
+        ,cd_contato
+        ,cd_pedido
+        ,sum(qt_item)                                         as qt_item
+        ,sum(vl_total_item)                                   as vl_total_item
+        ,sum(vl_desconto_rateio)                              as vl_desconto_rateio
+        ,sum(vl_frete_rateio)                                 as vl_frete_rateio
+        ,sum(vl_total_pedido)                                 as vl_total_pedido
+        ,sum(vl_custo_pedido)                                 as vl_custo_pedido
+        ,min(dt_pedido) over ()                               as dt_pedido_min
+        ,max(dt_pedido) over ()                               as dt_pedido_max
     from cte_pedido
-   where cd_status_pedido not in ('CANCELADO')
+   where ds_status_pedido not in ('CANCELADO')
 group by dt_pedido
+        ,cd_contato
+        ,cd_pedido
 )
 
 , cte_tempo as (
@@ -88,16 +91,34 @@ group by dt_pedido
           ,a.fg_dia_util
           ,a.fg_feriado
           ,a.ds_feriado
-          ,coalesce(b.qt_pedido, 0)          as qt_pedido
-          ,coalesce(b.qt_item, 0)            as qt_item
-          ,coalesce(b.vl_total_item, 0)      as vl_total_item
-          ,coalesce(b.vl_desconto_rateio, 0) as vl_desconto_rateio
-          ,coalesce(b.vl_frete_rateio, 0)    as vl_frete_rateio
-          ,coalesce(b.vl_total_pedido, 0)    as vl_total_pedido
-          ,coalesce(b.vl_custo_pedido, 0)    as vl_custo_pedido
+          ,b.cd_contato
+          ,b.cd_pedido
+          ,sum(coalesce(b.qt_item, 0))            as qt_item
+          ,sum(coalesce(b.vl_total_item, 0))      as vl_total_item
+          ,sum(coalesce(b.vl_desconto_rateio, 0)) as vl_desconto_rateio
+          ,sum(coalesce(b.vl_frete_rateio, 0))    as vl_frete_rateio
+          ,sum(coalesce(b.vl_total_pedido, 0))    as vl_total_pedido
+          ,sum(coalesce(b.vl_custo_pedido, 0))    as vl_custo_pedido
      from cte_tempo      as a
 left join cte_pedido_agg as b
        on cast(a.dt_data as date) = cast(b.dt_pedido as date)
+ group by a.dt_data
+         ,a.nr_ano
+         ,a.nr_mes
+         ,a.nr_dia
+         ,a.nr_semana
+         ,a.dt_prim_dia_mes
+         ,a.ds_dia_semana
+         ,a.ds_dia_semana_abreviado
+         ,a.ds_mes
+         ,a.ds_mes_abreviado
+         ,a.ds_trimestre
+         ,a.ds_semestre
+         ,a.fg_dia_util
+         ,a.fg_feriado
+         ,a.ds_feriado
+         ,b.cd_contato
+         ,b.cd_pedido
 )
 
   select *
